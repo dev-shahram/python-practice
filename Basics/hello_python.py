293c2af28a9491c6c31f64229a5749efaa463066
Shahram.dev <shahram.codes@gmail.com>
2026-06-26 22:06:13 +0500

    if else

--- Basics/hello_python.py
+++ Basics/hello_python.py
@@ -3,5 +3,19 @@ print("Hello, Python!")
 name = "Shahram"
 age = 20
 
-print("Name:", name)
-print("Age:", age)
+
+print("Hello, my name is", name, "and I am", age, "years old.")
+
+
+
+a=2;
+a=2.0;
+print(a)
+
+
+a=20
+b=90
+if a>b:
+    print("a is greater than b")
+else:
+    print("b is greater than a")
