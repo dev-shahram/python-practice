dd2f66b0f7c95fb21c60ac6e4b9b114c1f5e8008
Shahram.dev <shahram.codes@gmail.com>
2026-07-01 19:01:56 +0500

    sum of 3 digit program

---
+++ Basics/sumOf3Digit.py
@@ -0,0 +1,9 @@
+num=int(input("Enter a 3 digit number:"))
+#this % is a modulas opertor yay last digit nikalta ha 
+a=num%10
+# and thsi // is a floor division operator yay last digit nikalta ha
+num=num//10
+b=num%10
+num=num//10
+c=num%10
+print(a+b+c)
