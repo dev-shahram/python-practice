a40536f7ec9d3770cbc9aba637279725de378b22
Shahram.dev <shahram.codes@gmail.com>
2026-07-01 18:52:14 +0500

    membership operations

---
+++ Basics/memberShipOperations.py
@@ -0,0 +1,13 @@
+#in
+text = "Python"
+
+print("P" in text)
+print("z" in text)
+
+#return true false 
+
+#not in
+fruits = ["Apple", "Banana", "Mango"]
+
+print("Orange" not in fruits)
+print("Apple" not in fruits)
