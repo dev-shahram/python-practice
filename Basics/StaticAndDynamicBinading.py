eee53eb47ded267feb4b67032494bfe866a2bbd5
Shahram.dev <shahram.codes@gmail.com>
2026-07-01 10:59:39 +0500

    Static and synamic binding

---
+++ Basics/StaticAndDynamicBinading.py
@@ -0,0 +1,15 @@
+#first we talk about static typing and dynamic typing in python. Python is a dynamically typed language, which means that you don't have to declare the type of a variable when you create it. The type is determined at runtime based on the value assigned to the variable.
+#now we talk about static typing. Static typing is a feature of some programming languages where the type of a variable is known at compile time. This means that you have to declare the type of a variable when you create it, and the type cannot change throughout the program.
+#now we talk about static and dynamic binding. Static binding is a feature of some programming languages where the method to be called is determined at compile time based on the type of the object. This means that the method to be called is determined by the type of the reference variable, not the type of the object it refers to.
+#now we talk about dynamic binding. Dynamic binding is a feature of some programming languages where the method to be called is determined at runtime based on the type of the object. This means that the method to be called is determined by the type of the object, not the type of the reference variable.
+
+#dynamic binding:
+
+a=5
+print(type(a)) 
+a="shahram"
+print(type(a))
+
+#static binding:
+
+#it is in java and c++ but not in python. 
