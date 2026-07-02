1177b2f2e86f4bbafc6de5b1d2de556eb8986081
Shahram.dev <shahram.codes@gmail.com>
2026-07-02 09:18:47 +0500

    modules in python

---
+++ Basics/Modules.py
@@ -0,0 +1,13 @@
+import math
+print(math.sqrt(16))
+print(math.factorial(5))
+print(math.pow(2, 3))
+
+
+#keyword
+import keyword
+print(keyword.kwlist)
+
+#random
+import random
+print(random.randint(1, 10))
