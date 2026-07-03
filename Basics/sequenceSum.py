3e5991faa8315e8f0b9d1af18ac8a582aea1e8f2
Shahram.dev <shahram.codes@gmail.com>
2026-07-03 22:10:26 +0500

    #find sum of 1/1!+2/2!+3/3!+....Nth term

--- Basics/sequenceSum.py
+++ Basics/sequenceSum.py
@@ -3,7 +3,19 @@ n=int(input("enter n "))
 sum=0
 fact=1
 for i in range (1,n+1):
-    fact=fact*n
-    sum=sum+n/fact
+    fact=fact*i
+    sum=sum+i/fact
 
 print(sum)     
+
+
+### by using math 
+import math
+z=int(input("enter n "))
+su=0
+fac=1
+for i in range(1,z+1):
+     fac=math.factorial(i)
+     su=su+i/fac
+
+print(su)
