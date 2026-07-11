2fad4636dddca8639ca65e0d7d779d685ef9f855
Shahram.dev <shahram.codes@gmail.com>
2026-07-11 11:45:21 +0500

    nothing

---
+++ Basics/Temp.py
@@ -0,0 +1,12 @@
+import math
+num=int(input("enter your first num "))
+num2=int(input("enter your 2nd num "))
+num3=int(input("enter your 3rd num "))
+
+
+first=math.factorial(num)
+sec=math.factorial(num2)
+third=math.factorial(num3)
+
+
+print(first," ", sec," ",third)
