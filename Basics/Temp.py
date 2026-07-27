5143b1e5c3de8763a4e0bbba8d828349e01cd722
Shahram.dev <shahram.codes@gmail.com>
2026-07-27 21:33:29 +0500

    update

--- Basics/Temp.py
+++ Basics/Temp.py
@@ -4,4 +4,4 @@ if flag==True:
 else:
     print("Flag is set to False")
 
-    
+print("This is a simple Python script that checks the value of a boolean variable called 'flag'. If 'flag' is set to True, it prints ")    
