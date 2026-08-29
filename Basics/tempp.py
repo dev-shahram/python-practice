868e56d90ca26e9e539072b30a10a829c3ebe5d0
Shahram.dev <shahram.codes@gmail.com>
2026-08-29 21:25:45 +0500

    hf

--- Basics/tempp.py
+++ Basics/tempp.py
@@ -3,7 +3,7 @@ list=["ali","5","ahmad","3","khan","7"]
 
 for i in list:
     if i=="ahmad":
-        print("found ahmad at index",list.index(i))
+        print("found  ahmad at index",list.index(i))
         break
 
 sorted_list=sorted(list)
